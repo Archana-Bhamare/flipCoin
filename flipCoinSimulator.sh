a9b068222063b803656653ef2a5302cab33365aa
Archana-Bhamare <archusonar2@gmail.com>
2020-10-12 14:20:01 +0530

    Added : Flip coin multiple times

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,22 +1,13 @@
 #!/bin/bash -x
-
-
 read -p "Enter the range : " num
 
 for ((i=0;i<num;i++))
 do
 	toss=$((RANDOM%2))
-
-
 	if [ $toss -eq 0 ]
-
 	then
-
 		echo "Head Wins"
-
 	else
-
 		echo "Tail Wins"
-
 	fi
 done
