bc85b76eb589b0ef7502364471176d912cf16bd6
Archana-Bhamare <archusonar2@gmail.com>
2020-10-12 14:32:27 +0530

    Modify : Continue till anyone won by 21 times

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,13 +1,28 @@
 #!/bin/bash -x
-read -p "Enter the range : " num
 
-for ((i=0;i<num;i++))
+head=0
+tail=0
+
+while [[ $head -lt 21 && $tail -lt 21 ]]
 do
 	toss=$((RANDOM%2))
 	if [ $toss -eq 0 ]
 	then
-		echo "Head Wins"
-	else
-		echo "Tail Wins"
+		((head++))
+	elif [ $toss -eq 1 ]
+	then
+		((tail++))
+	fi
+	if [ $head -eq 21 ]
+	then
+		echo "Head Wins by $head times"
+	elif [ $tail -eq 21 ]
+	then
+		echo "Tail Wins by $tail times"
+	elif [[ $head -eq 21 && $echo -eq 21 ]]
+	then
+		echo "It's a Tie"
 	fi
 done
+echo "The number of Head was : $head"
+echo "The number of Tail was : $tail"
